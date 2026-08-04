9368e38f6f9dddb1a354fd8048af507c61ee6578
Hallucinogen Codex Agent <agent-codex@hallucinogen.local>
2026-08-04 18:07:27 +0000

    fix: extract confirm shell from nix output

--- outputs.nix
+++ outputs.nix
@@ -1,32 +1,60 @@
-{ inputs }: let
-  self = inputs.self;
-  nixpkgs = inputs.nixpkgs;
-  set-and-setting = inputs.set-and-setting;
-  cavekit-src = inputs.cavekit-src;
+{ inputs }:
+let
+  inherit (inputs) self;
+  inherit (inputs) nixpkgs;
+  inherit (inputs) set-and-setting;
+  inherit (inputs) cavekit-src;
 in
 {
-      packages = nixpkgs.lib.genAttrs [
+  packages =
+    nixpkgs.lib.genAttrs
+      [
         "aarch64-darwin"
         "x86_64-darwin"
         "x86_64-linux"
         "aarch64-linux"
-      ] (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-        default = pkgs.runCommand "cavekit-plugin" { } ''
-          cd ${cavekit-src}
-          bash ${./install-plugin.sh}
-        '';
-        setting = (set-and-setting.lib.mkSetting { inherit pkgs; }).materialized;
-      });
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.runCommand "cavekit-plugin" { } ''
+            cd ${cavekit-src}
+            bash ${./install-plugin.sh}
+          '';
+          setting = (set-and-setting.lib.mkSetting { inherit pkgs; }).materialized;
+        }
+      );
 
-      devShells = nixpkgs.lib.genAttrs [
+  devShells =
+    nixpkgs.lib.genAttrs
+      [
         "aarch64-darwin"
         "x86_64-darwin"
         "x86_64-linux"
         "aarch64-linux"
-      ] (system: let pkgs = nixpkgs.legacyPackages.${system}; in
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
         (set-and-setting.lib.mkDevShells {
           inherit pkgs;
-          basePackages = (set-and-setting.lib.materializationFor { inherit pkgs; fragments = [ "base" "nix" "shell" "ascii" "markdown" "yaml" ]; }).packages;
+          basePackages =
+            (set-and-setting.lib.materializationFor {
+              inherit pkgs;
+              fragments = [
+                "base"
+                "nix"
+                "shell"
+                "ascii"
+                "markdown"
+                "yaml"
+              ];
+            }).packages;
           settingHook = ''
               ${self.packages.${system}.setting}/bin/sync-setting .
             _assemble_out="$(mktemp -d)"
@@ -56,70 +84,98 @@ in
         }
       );
 
-      checks = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ] (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        let
-          sys = pkgs.stdenv.hostPlatform.system;
-          standardChecks = set-and-setting.lib.checksFor {
-            inherit pkgs;
-            fragments = [ "base" "nix" "shell" "ascii" "markdown" "yaml" ];
-            src = ./.;
-          };
-        in
-        standardChecks
-        // rec {
-          dep-graph = set-and-setting.lib.mkDepGraphCheck {
-            inherit pkgs;
-            projectRoot = ./.;
-          };
-          package = pkgs.runCommand "check-package" {
-            cavekitPkg = self.packages.${sys}.default;
-          } (builtins.readFile ./check-package.sh);
-          package-files = package;
-          install-validation = pkgs.runCommand "check-install-validation" {
-            installScript = builtins.readFile ./install-plugin.sh;
-          } (builtins.readFile ./check-install-validation.sh);
-          shellcheck-scripts = standardChecks.shellcheck;
-          shfmt-format = standardChecks.shfmt;
-          default = pkgs.runCommand "checks" { } "touch $out";
-        }
-      );
+  checks = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ] (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-      apps = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ] (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        let
-          mat = set-and-setting.lib.materializationFor {
-            inherit pkgs;
-            fragments = [ "base" "nix" "shell" "ascii" "markdown" "yaml" ];
-          };
-        in
-        {
-          confirm = {
-            type = "app";
-            program = "${
-              pkgs.writeShellApplication {
-                name = "confirm";
-                runtimeInputs = [
-                  pkgs.coreutils
-                  pkgs.diffutils
-                  pkgs.findutils
-                  pkgs.gawk
-                  pkgs.git
-                  pkgs.gnugrep
-                ]
-                ++ mat.packages;
-                text = ''
-                  export FRAGMENTS_DIR="${set-and-setting}/setting/integrations/lefthook"
-                  export ASSEMBLE_SCRIPT="${set-and-setting}/setting/lib/assemble-lefthook.sh"
-                  export DETECT_SCRIPT="${set-and-setting}/setting/lib/detect-fragments.sh"
-                  export SETTING_SRC="${self.packages.${pkgs.stdenv.hostPlatform.system}.setting}"
-                  export CONFIRM_SCRIPT="${set-and-setting}/lib/confirm.sh"
-                  export CONFIRM_REV="${set-and-setting.rev or "unknown"}"
-                  bash "$CONFIRM_SCRIPT"
-                '';
-              }
-            }/bin/confirm";
-          };
-        }
-      );
+      sys = pkgs.stdenv.hostPlatform.system;
+      standardChecks = set-and-setting.lib.checksFor {
+        inherit pkgs;
+        fragments = [
+          "base"
+          "nix"
+          "shell"
+          "ascii"
+          "markdown"
+          "yaml"
+        ];
+        src = ./.;
+      };
+    in
+    standardChecks
+    // rec {
+      dep-graph = set-and-setting.lib.mkDepGraphCheck {
+        inherit pkgs;
+        projectRoot = ./.;
+      };
+      package = pkgs.runCommand "check-package" {
+        cavekitPkg = self.packages.${sys}.default;
+      } (builtins.readFile ./check-package.sh);
+      package-files = package;
+      install-validation = pkgs.runCommand "check-install-validation" {
+        installScript = builtins.readFile ./install-plugin.sh;
+      } (builtins.readFile ./check-install-validation.sh);
+      shellcheck-scripts = standardChecks.shellcheck;
+      shfmt-format = standardChecks.shfmt;
+      default = pkgs.runCommand "checks" { } "touch $out";
     }
+  );
+
+  apps = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ] (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      mat = set-and-setting.lib.materializationFor {
+        inherit pkgs;
+        fragments = [
+          "base"
+          "nix"
+          "shell"
+          "ascii"
+          "markdown"
+          "yaml"
+        ];
+      };
+    in
+    {
+      confirm = {
+        type = "app";
+        program = "${
+          pkgs.writeShellApplication {
+            name = "confirm";
+            runtimeInputs = [
+              pkgs.coreutils
+              pkgs.diffutils
+              pkgs.findutils
+              pkgs.gawk
+              pkgs.git
+              pkgs.gnugrep
+            ]
+            ++ mat.packages;
+            text =
+              builtins.replaceStrings
+                [
+                  "@FRAGMENTS_DIR@"
+                  "@ASSEMBLE_SCRIPT@"
+                  "@DETECT_SCRIPT@"
+                  "@SETTING_SRC@"
+                  "@CONFIRM_SCRIPT@"
+                  "@CONFIRM_REV@"
+                ]
+                [
+                  "${set-and-setting}/setting/integrations/lefthook"
+                  "${set-and-setting}/setting/lib/assemble-lefthook.sh"
+                  "${set-and-setting}/setting/lib/detect-fragments.sh"
+                  "${self.packages.${pkgs.stdenv.hostPlatform.system}.setting}"
+                  "${set-and-setting}/lib/confirm.sh"
+                  (set-and-setting.rev or "unknown")
+                ]
+                (builtins.readFile ./confirm.sh);
+          }
+        }/bin/confirm";
+      };
+    }
+  );
+}
