d2e8a3bcd489028963d06fb6a7e61646606483b0
Hallucinogen Codex Agent <agent-codex@hallucinogen.local>
2026-08-11 13:15:23 +0000

    fix: wire cavekit package outputs

--- flake.nix
+++ flake.nix
@@ -10,6 +10,11 @@
     nixpkgs-lock.url = "github:pr0d1r2/nixpkgs-lock";
     nixpkgs.follows = "nixpkgs-lock/nixpkgs";
 
+    cavekit-src = {
+      url = "github:JuliusBrussee/cavekit";
+      flake = false;
+    };
+
     set-and-setting.url = "github:pr0d1r2/set-and-setting";
     set-and-setting.inputs.nixpkgs-lock.follows = "nixpkgs-lock";
   };
@@ -19,18 +24,12 @@
       self,
       nixpkgs,
       set-and-setting,
+      cavekit-src,
       ...
     }:
-    set-and-setting.lib.mkConsumerFlake {
-      inherit self nixpkgs set-and-setting;
-      fragments = [
-        "base"
-        "nix"
-        "shell"
-        "ascii"
-        "markdown"
-        "yaml"
-      ];
-      src = ./.;
+    import ./outputs.nix {
+      inputs = {
+        inherit self nixpkgs set-and-setting cavekit-src;
+      };
     };
 }
